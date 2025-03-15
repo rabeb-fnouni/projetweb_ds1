1dc4438872d39b66b9de1d743dc6df37ced34dcf
Rabeb Fnouni <roubyfnouni@gmail.com>
2025-03-15 23:11:03 +0100

    Delete views directory

--- views/errors/403.php
+++
@@ -1,5 +0,0 @@
-<?php require_once 'views\common\header.php'; ?>
-<h1>403 Forbidden</h1>
-<p>You do not have permission to access this page.</p>
-<p><a href="<?php echo BASE_URL; ?>/home">Return to Home</a></p>
-<?php require_once 'views\common\footer.php'; ?>
--- views/errors/404.php
+++
@@ -1,5 +0,0 @@
-<?php require_once 'views\common\header.php'; ?>
-<h1>404 Not Found</h1>
-<p>The page you are looking for could not be found.</p>
-<p><a href="<?php echo BASE_URL; ?>/home">Return to Home</a></p>
-<?php require_once 'views\common\footer.php'; ?>
